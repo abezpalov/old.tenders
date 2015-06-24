526e683ca5055f82656dc171d5996b5988ed98c0
Andrey Bezpalov <abezpalov@ya.ru>
2015-06-24 23:48:39 +0400

    Добавлены сущности Currency, OKEI, OKEIGroup, OKEISection

--- models.py
+++ models.py
@@ -28,6 +28,7 @@ class Updater(models.Model):
 	created  = models.DateTimeField()
 	modified = models.DateTimeField()
 	updated  = models.DateTimeField()
+
 	objects  = UpdaterManager()
 
 	def __str__(self):
@@ -60,6 +61,7 @@ class Country(models.Model):
 	state     = models.BooleanField(default = True)
 	created   = models.DateTimeField()
 	modified  = models.DateTimeField()
+
 	objects   = CountryManager()
 
 	def __str__(self):
@@ -93,6 +95,7 @@ class Region(models.Model):
 	created   = models.DateTimeField()
 	modified  = models.DateTimeField()
 	updated   = models.DateTimeField(null = True, default = None)
+
 	objects   = RegionManager()
 	
 	def __str__(self):
@@ -100,6 +103,247 @@ class Region(models.Model):
 
 	class Meta:
 		ordering = ['name']
+
+
+class CurrencyManager(models.Manager):
+
+	def take(self, code, digital_code, name = None, state = True):
+		try:
+			currency = self.get(code = code)
+		except Currency.DoesNotExist:
+			currency              = Currency()
+			currency.code         = code
+			currency.digital_code = digital_code
+			currency.name         = name
+			currency.state        = state
+			currency.created      = timezone.now()
+			currency.modified     = timezone.now()
+			currency.save()
+		return currency
+
+	def update(self, code, digital_code, name = None, state = True):
+		try:
+			currency = self.get(code = code)
+			currency.digital_code = digital_code
+			currency.name         = name
+			currency.state        = state
+			currency.modified     = timezone.now()
+			currency.save()
+		except Currency.DoesNotExist:
+			currency              = Currency()
+			currency.code         = code
+			currency.digital_code = digital_code
+			currency.name         = name
+			currency.state        = state
+			currency.created      = timezone.now()
+			currency.modified     = timezone.now()
+			currency.save()
+		return currency
+
+
+class Currency(models.Model):
+	code         = models.CharField(max_length = 10, unique = True)
+	digital_code = models.CharField(max_length = 10, unique = True)
+	name         = models.CharField(max_length = 100, null = True, default = None)
+	state        = models.BooleanField(default = True)
+	created      = models.DateTimeField()
+	modified     = models.DateTimeField()
+
+	objects      = CurrencyManager()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['code']
+
+
+class OKEISectionManager(models.Manager):
+
+	def take(self, code, name = None, state = True):
+		try:
+			okei_section = self.get(code = code)
+		except OKEISection.DoesNotExist:
+			okei_section          = OKEISection()
+			okei_section.code     = code
+			okei_section.name     = name
+			okei_section.state    = state
+			okei_section.created  = timezone.now()
+			okei_section.modified = timezone.now()
+			okei_section.save()
+		return okei_section
+
+	def update(self, code, name = None, state = True):
+		try:
+			okei_section          = self.get(code = code)
+			okei_section.name     = name
+			okei_section.state    = state
+			okei_section.modified = timezone.now()
+			okei_section.save()
+		except OKEISection.DoesNotExist:
+			okei_section          = OKEISection()
+			okei_section.code     = code
+			okei_section.name     = name
+			okei_section.state    = state
+			okei_section.created  = timezone.now()
+			okei_section.modified = timezone.now()
+			okei_section.save()
+		return okei_section
+
+
+class OKEISection(models.Model):
+	code     = models.CharField(max_length = 10, unique = True)
+	name     = models.CharField(max_length = 100, null = True, default = None)
+	state    = models.BooleanField(default = True)
+	created  = models.DateTimeField()
+	modified = models.DateTimeField()
+
+	objects  = OKEISectionManager()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['code']
+
+
+class OKEIGroupManager(models.Manager):
+
+	def take(self, code, name = None, state = True):
+		try:
+			okei_group = self.get(code = code)
+		except OKEIGroup.DoesNotExist:
+			okei_group          = OKEIGroup()
+			okei_group.code     = code
+			okei_group.name     = name
+			okei_group.state    = state
+			okei_group.created  = timezone.now()
+			okei_group.modified = timezone.now()
+			okei_group.save()
+		return okei_group
+
+	def update(self, code, name = None, state = True):
+		try:
+			okei_group          = self.get(code = code)
+			okei_group.name     = name
+			okei_group.state    = state
+			okei_group.modified = timezone.now()
+			okei_group.save()
+		except OKEIGroup.DoesNotExist:
+			okei_group          = OKEIGroup()
+			okei_group.code     = code
+			okei_group.name     = name
+			okei_group.state    = state
+			okei_group.created  = timezone.now()
+			okei_group.modified = timezone.now()
+			okei_group.save()
+		return okei_group
+
+
+class OKEIGroup(models.Model):
+	code     = models.CharField(max_length = 10, unique = True)
+	name     = models.CharField(max_length = 100, null = True, default = None)
+	state    = models.BooleanField(default = True)
+	created  = models.DateTimeField()
+	modified = models.DateTimeField()
+	objects  = OKEIGroupManager()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['code']
+
+
+class OKEIManager(models.Manager):
+
+	def take(self, code, full_name = None, section = None, group = None, local_name = None, international_name = None, local_symbol = None, international_symbol = None, state = True):
+		try:
+			okei = self.get(code = code)
+		except OKEI.DoesNotExist:
+			okei                      = OKEI()
+			okei.code                 = code
+			okei.full_name            = full_name
+			okei.section              = section
+			okei.group                = group
+			okei.local_name           = local_name
+			okei.international_name   = international_name
+			okei.local_symbol         = local_symbol
+			okei.international_symbol = international_symbol
+			okei.state                = state
+			okei.created              = timezone.now()
+			okei.modified             = timezone.now()
+			okei.save()
+		return okei
+
+	def update(self, code, full_name = None, section = None, group = None, local_name = None, international_name = None, local_symbol = None, international_symbol = None, state = True):
+		try:
+			okei                      = self.get(code = code)
+			okei.full_name            = full_name
+			okei.section              = section
+			okei.group                = group
+			okei.local_name           = local_name
+			okei.international_name   = international_name
+			okei.local_symbol         = local_symbol
+			okei.international_symbol = international_symbol
+			okei.state                = state
+			okei.modified             = timezone.now()
+			okei.save()
+		except OKEI.DoesNotExist:
+			okei                      = OKEI()
+			okei.code                 = code
+			okei.full_name            = full_name
+			okei.section              = section
+			okei.group                = group
+			okei.local_name           = local_name
+			okei.international_name   = international_name
+			okei.local_symbol         = local_symbol
+			okei.international_symbol = international_symbol
+			okei.state                = state
+			okei.created              = timezone.now()
+			okei.modified             = timezone.now()
+			okei.save()
+		return okei
+
+
+class OKEI(models.Model):
+	code                 = models.CharField(max_length = 10, unique = True)
+	full_name            = models.CharField(max_length = 100, null = True, default = None)
+	section              = models.ForeignKey(OKEISection, null = True, default = None)
+	group                = models.ForeignKey(OKEIGroup, null = True, default = None)
+	local_name           = models.CharField(max_length = 100, null = True, default = None)
+	international_name   = models.CharField(max_length = 100, null = True, default = None)
+	local_symbol         = models.CharField(max_length = 100, null = True, default = None)
+	international_symbol = models.CharField(max_length = 100, null = True, default = None)
+	state                = models.BooleanField(default = True)
+	created              = models.DateTimeField()
+	modified             = models.DateTimeField()
+
+	objects              = OKEIManager()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ['code']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- updaters/zakupki-gov-ru.py
+++ updaters/zakupki-gov-ru.py
@@ -1,19 +1,18 @@
-import time
-from datetime import date
-from django.utils import timezone
 from tenders.models import *
 
 
 class Runner:
 
+
 	name = 'Обновление с сайта государственных закупок России'
 	alias = 'zakupki-gov-ru'
-
 	urls = {
-		'base':    'ftp.zakupki.gov.ru',
-		'lists':   'fcs_nsi',
-		'regions': 'fcs_regions'
-	}
+		'base':     'ftp.zakupki.gov.ru',
+		'essences': 'fcs_nsi',
+		'regions':  'fcs_regions',
+		'currency': 'nsiCurrency',
+		'okei':     'nsiOKEI'}
+
 
 	def __init__(self):
 
@@ -22,6 +21,7 @@ class Runner:
 			alias = self.alias,
 			name  = self.name)
 
+
 	def run(self):
 
 		# Проверяем наличие параметров авторизации
@@ -29,41 +29,27 @@ class Runner:
 			print('Ошибка: Проверьте параметры авторизации. Кажется их нет.')
 			return False
 
-		print("login: {}".format(self.updater.login))
-		print("password: {}".format(self.updater.password))
+
+		# Обновляем справочники
+#		self.getEssencesList()                                                  # Тест
+		self.updateEssence('currency')
+		self.updateEssence('okei')
 
 
-		# Получаем список регионов с FTP-сервера
-		regions = self.getFTPCatalog('fcs_regions')
-		print(regions)
-		print('\n')
+		# Обновляем список регионов
+		self.updateRegions()
 
+		# Обновляем планы регионов
+		# TODO
 
-		for region in regions:
-
-			if region != '_logs':
-				region = Region.objects.take(
-					alias = region,
-					name = region,
-					full_name = region)
-
-				if region.state:
-
-					# Ждем, чтобы не получить отбой сервера
-					time.sleep(10)
-
-					# Получаем содержимое каталога
-					content = self.getFTPCatalog('{}/{}'.format(self.urls['regions'], region.alias))
-					print(region.name)
-					print(content)
-					print('\n')
-
+		# Обновляем тендеры регионов
 		# TODO
 
 		return True
 
 
 	def getFTPCatalog(self, catalog = None):
+		'Возврящает содержимое папки'
 
 		# Импортируем
 		from ftplib import FTP
@@ -86,6 +72,265 @@ class Runner:
 		return result
 
 
+	def getZipFromFTP(self, file_name, catalog = None):
+		'Скачивает и возвращает файл с FTP-сервера'
 
+		# Импортируем
+		from ftplib import FTP
+		from io import BytesIO
+		from zipfile import ZipFile
+
+		# Инициализируем переменные
+		ftp = FTP(host = self.urls['base'])
+		zip_file = BytesIO(b"")
+
+		# Авторизуемся
+		ftp.login(user = self.updater.login, passwd = self.updater.password)
+
+		# Переходим в нужный каталог
+		if catalog:
+			ftp.cwd(catalog)
+
+		# Скачиваем файл
+		ftp.retrbinary("RETR {}".format(file_name), zip_file.write)
+		zip_data  = ZipFile(zip_file)
+
+		# Возвращаем результат
+		return zip_data
+
+
+	def getEssencesList(self):
+		'Получает список сущностей для тестирования'
+
+		essences = self.getFTPCatalog(self.urls['essences'])
+
+		return essences
+
+
+	def updateEssence(self, essence):
+		'Получает файлы сущностей для анализа и обработки'
+
+		# Импортируем
+		from io import TextIOWrapper
+
+		# Получаем список файлов
+		catalog = "/{}/{}".format(self.urls['essences'], self.urls[essence])
+		zip_names = self.getFTPCatalog(catalog)
+
+		# Загружаем архивы
+		for zip_name in zip_names:
+
+			# Скачиваем архив
+			zip_data = self.getZipFromFTP(zip_name, catalog)
+			print("Скачал файл {}, тип объекта {}.".format(zip_name, type(zip_data)))
+
+			# Получаем список файлов в архиве
+			xml_names = zip_data.namelist()
+			for xml_name in xml_names:
+
+				# Извлекаем файл из архива
+				xml_data = zip_data.open(xml_name)
+				print("Извлек файл {} типа {} из архива {}.".format(xml_name, type(xml_data), zip_name))
+
+				# Парсим файл
+				if   'currency' == essence: self.parseCurrency(xml_data)
+				elif 'okei' == essence: self.parseOKEI(xml_data)
+				# TODO
+				# TODO
+				# TODO
+				# TODO
+				# TODO
+				else: print('Ошибка: отсутствует парсер для сущности {}.'.format(essence))
+
+				print("Файл {} обработан.".format(xml_name))
+
+		return True
+
+
+	def parseCurrency(self, xml_data):
+		'Парсит валюты'
+
+		# Импортируем
+		from lxml import etree
+
+		# Парсим
+		tree = etree.parse(xml_data)
+
+		# Получаем корневой элемент
+		root = tree.getroot()
+
+		# Получаем список
+		for currency_list in root:
+
+			# Получаем элемент
+			for currency in currency_list:
+
+				# Инициируем пустой справочник элемента
+				e = {}
+
+				# Обрабатываем значения полей
+				for value in currency:
+					if   value.tag.endswith('code'):
+						e['code'] = value.text
+					elif value.tag.endswith('digitalCode'):
+						e['digital_code'] = value.text
+					elif value.tag.endswith('name'):
+						e['name'] = value.text
+					elif value.tag.endswith('actual'):
+						if value.text == 'true':
+							e['state'] = True
+						else:
+							e['state'] = False
+
+				# Обновляем информацию в базе
+				currency = Currency.objects.update(
+					code         = e['code'],
+					digital_code = e['digital_code'],
+					name         = e['name'],
+					state        = e['state'])
+
+				print("Обновлена валюта: {}.".format(currency.code))
+
+		return True
+
+
+	def parseOKEI(self, xml_data):
+		'Парсит единицы измерения'
+
+		# Импортируем
+		from lxml import etree
+
+		# Парсим
+		tree = etree.parse(xml_data)
+
+		# Получаем корневой элемент
+		root = tree.getroot()
+
+		# Получаем список
+		for okei_list in root:
+
+			# Получаем элемент
+			for okei in okei_list:
+
+				# Инициируем пустой справочник элемента
+				e = {}
+
+				# Обрабатываем значения полей
+				for value in okei:
+
+					# code
+					if value.tag.endswith('code'):
+						e['code'] = value.text
+
+					# full_name
+					elif value.tag.endswith('fullName'):
+						e['full_name'] = value.text
+
+					# section
+					elif value.tag.endswith('section'):
+
+						for section_value in value:
+
+							if section_value.tag.endswith('code'):
+								e['section_code'] = section_value.text
+							elif section_value.tag.endswith('name'):
+								e['section_name'] = section_value.text
+
+					# group
+					elif value.tag.endswith('group'):
+
+						for group_value in value:
+
+							if group_value.tag.endswith('id'):
+								e['group_code'] = group_value.text
+							elif group_value.tag.endswith('name'):
+								e['group_name'] = group_value.text
+
+					# local_name
+					elif value.tag.endswith('localName'):
+						e['local_name'] = value.text
+
+					# international_name
+					elif value.tag.endswith('internationalName'):
+						e['international_name'] = value.text
+
+					# local_symbol
+					elif value.tag.endswith('localSymbol'):
+						e['local_symbol'] = value.text
+
+					# international_symbol
+					elif value.tag.endswith('internationalSymbol'):
+						e['international_symbol'] = value.text
+
+					# state
+					elif value.tag.endswith('actual'):
+						if value.text == 'true':
+							e['state'] = True
+						else:
+							e['state'] = False
+
+				# Обновляем информацию в базе
+				okei_section = OKEISection.objects.update(
+					code         = e['section_code'],
+					name         = e['section_name'],
+					state        = True)
+				okei_group = OKEIGroup.objects.update(
+					code         = e['group_code'],
+					name         = e['group_name'],
+					state        = True)
+				okei = OKEI.objects.update(
+					code                 = e['code'],
+					full_name            = e['full_name'],
+					section              = okei_section,
+					group                = okei_group,
+					local_name           = e['local_name'],
+					international_name   = e['international_name'],
+					local_symbol         = e['local_symbol'],
+					international_symbol = e['international_symbol'],
+					state                = True)
+
+				print("Обновлена единица измерения: {}.".format(okei.full_name))
+
+		return True
+
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+	# TODO Parser
+
+
+	def updateRegions(self):
+		'Обновляет список регионов'
+
+		# Черный лист
+		black_list = ['_logs']
+
+		# Получаем список регионов с FTP-сервера
+		regions = self.getFTPCatalog(self.urls['regions'])
+		print('Получен список регионов.')
+
+		for region in regions:
+
+			if not region in black_list:
+				region = Region.objects.take(
+					alias = region,
+					name = region,
+					full_name = region)
+
+		return True
 
 
