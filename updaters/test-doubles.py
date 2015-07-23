105ac96f4357fd104e3cf81fb821670929730db2
Andrey Bezpalov <abezpalov@ya.ru>
2015-07-23 21:08:32 +0400

    Переделал тест на поблоковый

--- updaters/test-doubles.py
+++ updaters/test-doubles.py
@@ -22,23 +22,42 @@ class Runner:
 
 		start = timezone.now()
 
-		products = PlanGraphPositionProduct.objects.all()
+		products_count = PlanGraphPositionProduct.objects.all().count()
 
-		for n, product in enumerate(products):
+		#
+		print(products_count)
 
-			test = PlanGraphPositionProduct.objects.filter(
-				position = product.position,
-				number = product.number)
+		items_on_page   = 1000
 
-			if len(test) > 1:
-				log = Log.objects.add(
-					subject = "Tenders SQL",
-					channel = "Critical Error",
-					title = "Double Product: position.id = {}, number = {}.".format(position.id, number))
+		page_max = products_count // items_on_page
+		if products_count % items_on_page:
+			page_max += 1
 
-				print(log)
-			else:
-				print("Продукт {} из {}. Чисто.".format(n, len(products)))
+		#
+		print(page_max)
+
+		for pn in range(0, page_max):
+			products = PlanGraphPositionProduct.objects.all()[pn * items_on_page : (pn + 1) * items_on_page]
+
+			print(len(products))
+
+			for n, product in enumerate(products):
+
+				test = PlanGraphPositionProduct.objects.filter(
+					position = product.position,
+					number = product.number)
+
+				if len(test) > 1:
+					log = Log.objects.add(
+						subject = "Tenders SQL",
+						channel = "Critical Error",
+						title = "Double Product: position.id = {}, number = {}.".format(position.id, number))
+
+					print(log)
+
+				else:
+
+					print("Продукт {}{} из {}. Чисто.".format(pn, n, products_count))
 
 		print("Обработка завершена за {}.".format(timezone.now() - start))
 
