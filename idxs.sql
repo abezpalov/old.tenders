51c8069d99bec1f82f693173299662dfa6b59016
Andrey Bezpalov <abezpalov@ya.ru>
2015-08-27 12:16:05 +0400

    Добавлены рекомендуемые индексы для базы

---
+++ idxs.sql
@@ -0,0 +1,9 @@
+﻿CREATE INDEX tenders_plangraphposition_state_idx
+	ON tenders_plangraphposition
+	USING btree
+	(state);
+
+CREATE INDEX tenders_plangraphposition_number_idx
+	ON tenders_plangraphposition
+	USING btree
+	(number);
